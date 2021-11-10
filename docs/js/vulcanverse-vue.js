cc33779cc7b0d7a9a4f83a4dd29537e68d31be31
Dael Separa <dael.separa@gmail.com>
2021-11-10 14:52:30 +0800

    codewords

--- docs/js/vulcanverse-vue.js
+++ docs/js/vulcanverse-vue.js
@@ -1,12 +1,21 @@
-
 var adventurer = { book: 2, name: '', companion: '', attributes: { charm: -2, grace: -2, ingenuity: -2, strength: -2 }, wounded: false, titles: [], money: 0, god: '', glory: 0, scars: 0, possessions: [], blessings: [], location: 1, codewords: [], notes: '', ticks: [] };
+
+var codewords1 = ['Nadir', 'Namesake', 'Nanny', 'Napalm', 'Nasty', 'Natron', 'Nature', 'Naughty', 'Nautilus', 'Navarine', 'Nefarious', 'Negate', 'Nemesis', 'Neophyte', 'Nephew', 'Nervous', 'Nest', 'Nettle', 'Neural', 'Neutralised', 'Neveragain', 'Neverending', 'Negate', 'Nemesis', 'Neophyte', 'Nephew', 'Nervous', 'Nest', 'Nettle', 'Neural', 'Neutralised', 'Neveragain', 'Neverending', 'Nimbus', 'Nirvana', 'Noble', 'Noisome', 'Nomad', 'Noodles', 'Nought', 'Nullify', 'Numb', 'Nurture'];
+var codewords2 = ['Oasis', 'Oblige', 'Ochre', 'Ode', 'Oedipus', 'Offer', 'Ogle', 'Ohone', 'Oil', 'Okra', 'Olifant', 'Omen', 'Onerous', 'Ooze', 'Optics', 'Oquassa', 'Ordeal', 'Ostrich', 'Other', 'Outburst', 'Ovation', 'Owl', 'Oxen', 'Oyster', 'Ozone'];
+var codewords3 = [];
+var codewords4 = [];
+var codewords5 = [];
+var codewords6 = [];
+
+var codewords = [[], codewords1, codewords2, codewords3, codewords4, codewords5];
 
 const VulcanVerseVueApp = {
 
     data() {
         return {
             pageTitle: 'VulcanVerse Adventure Sheet',
-            data: adventurer
+            data: adventurer,
+            codewords: codewords
         }
     },
     methods: {
@@ -20,15 +29,29 @@ const VulcanVerseVueApp = {
             } else if (adventurer.book == 2) {
                 data.map = 'images/desert-of-notus-map.jpg';
                 data.mapName = 'The Desert of Notus';
-            }
-            else {
+            } else {
                 data.mapName = 'Terra Incognito';
             }
 
             return data;
+        },
+
+        hasCodeword: function (codes, book, code) {
+
+            return (codes.find(e => e.book == book && e.code == code) !== undefined);
+        },
+
+        toggleCodeword: function (codes, book, code) {
+
+            if (this.hasCodeword(codes, book, code)) {
+                codes = codes.filter(e => e.book != book || e.code != code);
+            } else {
+                codes.push({ book: book, code: code });
+            }
+
+            return codes;
         }
     }
 }
 
-Vue.createApp(VulcanVerseVueApp).mount('#vulcanverse-vue');
-
+var app = Vue.createApp(VulcanVerseVueApp).mount('#vulcanverse-vue');
