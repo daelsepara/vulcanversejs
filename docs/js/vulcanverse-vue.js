66c62896113e14984b3e1e0478dba001e6e14b1e
Dael Separa <dael.separa@gmail.com>
2021-11-10 16:46:35 +0800

    numerous fixes. save/load adventure sheet (JSON)

--- docs/js/vulcanverse-vue.js
+++ docs/js/vulcanverse-vue.js
@@ -1,38 +1,40 @@
-var adventurer = { book: 2, name: '', companion: '', attributes: { charm: -2, grace: -2, ingenuity: -2, strength: -2 }, wounded: false, titles: [], money: 0, god: '', glory: 0, scars: 0, possessions: [], blessings: [], location: 1, codewords: [], notes: '', ticks: [] };
-
-var codewords1 = ['Nadir', 'Namesake', 'Nanny', 'Napalm', 'Nasty', 'Natron', 'Nature', 'Naughty', 'Nautilus', 'Navarine', 'Nefarious', 'Negate', 'Nemesis', 'Neophyte', 'Nephew', 'Nervous', 'Nest', 'Nettle', 'Neural', 'Neutralised', 'Neveragain', 'Neverending', 'Negate', 'Nemesis', 'Neophyte', 'Nephew', 'Nervous', 'Nest', 'Nettle', 'Neural', 'Neutralised', 'Neveragain', 'Neverending', 'Nimbus', 'Nirvana', 'Noble', 'Noisome', 'Nomad', 'Noodles', 'Nought', 'Nullify', 'Numb', 'Nurture'];
-var codewords2 = ['Oasis', 'Oblige', 'Ochre', 'Ode', 'Oedipus', 'Offer', 'Ogle', 'Ohone', 'Oil', 'Okra', 'Olifant', 'Omen', 'Onerous', 'Ooze', 'Optics', 'Oquassa', 'Ordeal', 'Ostrich', 'Other', 'Outburst', 'Ovation', 'Owl', 'Oxen', 'Oyster', 'Ozone'];
-var codewords3 = [];
-var codewords4 = [];
-var codewords5 = [];
-var codewords6 = [];
-
-var codewords = [[], codewords1, codewords2, codewords3, codewords4, codewords5];
-
 const VulcanVerseVueApp = {
 
     data() {
         return {
             pageTitle: 'VulcanVerse Adventure Sheet',
-            data: adventurer,
-            codewords: codewords
+            data: { book: 2, name: '', companion: '', attributes: { charm: -2, grace: -2, ingenuity: -2, strength: -2 }, wounded: false, titles: [], money: 0, god: '', glory: 0, scars: 0, possessions: [], blessings: [], location: 1, codewords: [], notes: '', ticks: [] },
+            codewords: [[], ['Nadir', 'Namesake', 'Nanny', 'Napalm', 'Nasty', 'Natron', 'Nature', 'Naughty', 'Nautilus', 'Navarine', 'Nefarious', 'Negate', 'Nemesis', 'Neophyte', 'Nephew', 'Nervous', 'Nest', 'Nettle', 'Neural', 'Neutralised', 'Neveragain', 'Neverending', 'Negate', 'Nemesis', 'Neophyte', 'Nephew', 'Nervous', 'Nest', 'Nettle', 'Neural', 'Neutralised', 'Neveragain', 'Neverending', 'Nimbus', 'Nirvana', 'Noble', 'Noisome', 'Nomad', 'Noodles', 'Nought', 'Nullify', 'Numb', 'Nurture'], ['Oasis', 'Oblige', 'Ochre', 'Ode', 'Oedipus', 'Offer', 'Ogle', 'Ohone', 'Oil', 'Okra', 'Olifant', 'Omen', 'Onerous', 'Ooze', 'Optics', 'Oquassa', 'Ordeal', 'Ostrich', 'Other', 'Outburst', 'Ovation', 'Owl', 'Oxen', 'Oyster', 'Ozone'], [], [], []]
         }
     },
     methods: {
+
         bookTitle: function () {
 
             var title = 'VulcanVerse Gamebook';
 
-            if (adventurer.book == 1) {
-                title = 'The Houses of the Dead';
-            } else if (adventurer.book == 2) {
-                title = 'The Hammer of the Sun';
-            } else if (adventurer.book == 3) {
-                title = 'The Wild Woods';
-            } else if (adventurer.book == 4) {
-                title = 'The Pillars of the Sky';
-            } else if (adventurer.book == 5) {
-                title = 'Workshop of the Gods';
+            if (this.data.book !== undefined) {
+
+                if (this.data.book == 1) {
+
+                    title = 'The Houses of the Dead';
+
+                } else if (this.data.book == 2) {
+
+                    title = 'The Hammer of the Sun';
+
+                } else if (this.data.book == 3) {
+
+                    title = 'The Wild Woods';
+
+                } else if (this.data.book == 4) {
+
+                    title = 'The Pillars of the Sky';
+
+                } else if (this.data.book == 5) {
+
+                    title = 'Workshop of the Gods';
+                }
             }
 
             return title;
@@ -40,47 +42,82 @@ const VulcanVerseVueApp = {
 
         selectMap: function () {
 
-            var data = { map: '', mapName: '' };
+            var mapData = { map: '', mapName: 'Terra Incognito' };
 
-            if (adventurer.book == 1) {
-                data.map = 'images/underworld-of-hades-map.jpg';
-                data.mapName = 'The Underworld of Hades';
-            } else if (adventurer.book == 2) {
-                data.map = 'images/desert-of-notus-map.jpg';
-                data.mapName = 'The Desert of Notus';
-            } else if (adventurer.book == 5) {
-                data.map = 'images/vulcan-city-map.jpg';
-                data.mapName = 'Vulcan City';
-            } else {
-                data.mapName = 'Terra Incognito';
+            if (this.data.book !== undefined) {
+
+                if (this.data.book == 1) {
+
+                    mapData.map = 'images/underworld-of-hades-map.jpg';
+                    mapData.mapName = 'The Underworld of Hades';
+                } else if (this.data.book == 2) {
+
+                    mapData.map = 'images/desert-of-notus-map.jpg';
+                    mapData.mapName = 'The Desert of Notus';
+
+                } else if (this.data.book == 5) {
+
+                    mapData.map = 'images/vulcan-city-map.jpg';
+                    mapData.mapName = 'Vulcan City';
+                }
             }
 
-            return data;
+            return mapData;
         },
 
-        hasCodeword: function (codes, book, code) {
+        hasCodeword: function (book, code) {
 
-            return (codes.find(e => e.book == book && e.code == code) !== undefined);
-        },
+            if (this.data.codewords !== undefined) {
 
-        toggleCodeword: function (codes, book, code) {
-
-            if (this.hasCodeword(codes, book, code)) {
-                codes = codes.filter(e => e.book != book || e.code != code);
-            } else {
-                codes.push({ book: book, code: code });
+                return (this.data.codewords.find(e => e.book == book && e.code == code) !== undefined);
             }
 
-            return codes;
+            return false;
         },
 
-        saveFile: function (adventurer) {
-            const data = JSON.stringify(adventurer)
-            const blob = new Blob([data], { type: 'application/json' })
+        toggleCodeword: function (book, code) {
+
+            if (this.hasCodeword(book, code)) {
+
+                this.data.codewords = this.data.codewords.filter(e => e.book != book || e.code != code);
+
+            } else {
+
+                this.data.codewords.push({ book: book, code: code });
+            }
+        },
+
+        saveFile: function () {
+
+            const json = JSON.stringify(this.data)
+
+            const blob = new Blob([json], { type: 'application/json' })
+
             var a = document.createElement("a");
+
             a.href = URL.createObjectURL(blob);
+
             a.download = 'adventurer.json';
+
             a.click();
+        },
+
+        loadFile: function (ev) {
+
+            const file = ev.target.files[0];
+
+            const reader = new FileReader();
+
+            reader.onload = e => {
+
+                var json = JSON.parse(e.target.result);
+
+                if (json !== undefined) {
+                    this.data = json;
+                }
+            };
+
+            reader.readAsText(file);
         }
     }
 }
